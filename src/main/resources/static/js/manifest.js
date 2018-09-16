11c246a0aad878edd7294f36bdb4a3ff30597d58
OldPudding <sacha-lee@qq.com>
2018-09-16 10:15:45 +0900

    first version

---
+++ src/main/resources/static/js/manifest.js
@@ -0,0 +1,2 @@
+alert("I Love Flutter");
+document.getElementById("text01").value="xiaoma";
---
+++ src/main/resources/static/js/test.user.js
@@ -0,0 +1,4 @@
+/**
+ * this is a java script plugin for chrome
+ */
+console.log("this is a test plugin");
